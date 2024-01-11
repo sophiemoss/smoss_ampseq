3a1928ea1f00e437a38f948aa53f9922f2e45577
Sophie Moss <77837798+sophiemoss@users.noreply.github.com>
2024-01-11 15:23:25 +0000

    Revert "merging main into apple"

--- pfalciparum_wgs_sade/coverage.sh
+++
@@ -1,3 +0,0 @@
-ls *.coverage | parallel --keep-order -j 60 --bar 'cat {} | awk '"'"'{if($3>=5)print}'"'"' | wc -l' > coverage_5
-ls *.coverage | parallel --keep-order -j 60 --bar 'cat {} | awk '"'"'{if($3>=10)print}'"'"' | wc -l' > coverage_10
-ls *.coverage | parallel --keep-order -j 60 --bar 'cat {} | awk '"'"'{if($3>=20)print}'"'"' | wc -l' > coverage_20
